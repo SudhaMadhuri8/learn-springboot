abee32ae3bb669b8e1b25c5574fb1b263f1f91a7
sudhamadhurikomrelli <sudhamadhurikomrelli@gmail.com>
2023-06-24 12:54:41 -0500

    Initial commit

---
+++ src/main/java/org/example/AddingTwoNum.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class AddingTwoNum {
+
+    private long num1;
+    private long num2;
+
+    public long getNum1() {
+        return num1;
+    }
+
+    public long getNum2() {
+        return num2;
+    }
+}
